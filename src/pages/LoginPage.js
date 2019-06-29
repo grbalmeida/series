6ff4937f96c590b82a0c04a512dcc64a161140a4
root <g.r.almeida@live.com>
2019-06-29 17:12:45 -0300

    Making the inputs work

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,17 +5,38 @@ import FormRow from '../components/FormRow'
 import Input from '../components/Input'
 
 export default class LoginPage extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  onChangeHandler (field, value) {
+    this.setState({
+      [field]: value
+    })
+  }
+
   render () {
+    const { email, password } = this.state
+
     return (
       <View>
         <FormRow>
           <Input
             placeholder='Email'
+            value={email}
+            onChangeText={email => this.onChangeHandler('email', email)}
           />
         </FormRow>
         <FormRow>
           <Input
             placeholder='Password'
+            value={password}
+            onChangeText={password => this.onChangeHandler('password', password)}
             secureTextEntry
           />
         </FormRow>
