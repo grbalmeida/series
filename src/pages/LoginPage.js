78d7f1babf6c1dc469f0ea4c7b54c9bdfa6b6dca
root <g.r.almeida@live.com>
2019-06-30 15:20:43 -0300

    Adding a new user

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,7 +3,8 @@ import {
   View,
   Button,
   StyleSheet,
-  ActivityIndicator
+  ActivityIndicator,
+  Alert
 } from 'react-native'
 import firebase from '../services/firebase'
 
@@ -29,6 +30,18 @@ export default class LoginPage extends Component {
     })
   }
 
+  showAlert () {
+    Alert.alert(
+      'User not found',
+      'Do you want to insert a new user?',
+      [
+        { text: 'Not', style: 'cancel' }, // style - only IOS
+        { text: 'Yes', onPress: () => this.createUserWithEmailAndPassword() }
+      ],
+      { cancelable: false }
+    )
+  }
+
   tryLogin () {
     this.setState({ isLoading: true })
 
@@ -38,19 +51,41 @@ export default class LoginPage extends Component {
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then(user => {
-        console.log(user)
         this.setState({ errorCode: '' })
       })
       .catch(error => {
+        if (error.code === 'auth/user-not-found') {
+          this.showAlert()
+        }
+
         this.setState({ errorCode: error.code })
       })
       .finally(() => this.setState({ isLoading: false }))
   }
 
+  createUserWithEmailAndPassword () {
+    const { email, password } = this.state
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        this.setState({ errorCode: '' })
+      })
+      .catch(error => {
+        this.setState({
+          errorCode: error.code
+        })
+      })
+  }
+
   getMessageByErrorCode (errorCode) {
     const messages = {
+      'auth/invalid-email': 'Invalid email',
       'auth/wrong-password': 'Incorret password',
-      'auth/user-not-found': 'User not found'
+      'auth/user-not-found': 'User not found',
+      'auth/weak-password': 'Weak password',
+      'auth/email-already-in-use': 'Email already exists'
     }
 
     return messages[errorCode] || 'Incorrect email or password'
