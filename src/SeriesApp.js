456f6f22d620ada4820cfd4bf727612e40a77b1f
root <g.r.almeida@live.com>
2019-06-30 18:06:42 -0300

    Creating the store

--- src/SeriesApp.js
+++ src/SeriesApp.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import rootReducer from './redux/reducers'
 import Router from './Router'
 
+const store = createStore(rootReducer)
+
 const SeriesApp = () => (
-  <Router />
+  <Provider store={store}>
+    <Router />
+  </Provider>
 )
 
 export default SeriesApp
