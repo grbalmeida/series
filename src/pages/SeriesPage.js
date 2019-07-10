27eec31ba9c56a8a3d6d5e6c194e14016f10bf33
root <g.r.almeida@live.com>
2019-07-10 19:50:59 -0300

    Finishing the layout of the SeriesPage

--- src/pages/SeriesPage.js
+++ src/pages/SeriesPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, FlatList } from 'react-native'
+import { View, FlatList, StyleSheet } from 'react-native'
 
 import SerieCard from '../components/SerieCard'
 import series from '../fake-data/series.json'
@@ -17,8 +17,27 @@ const SeriesPage = () => (
       )}
       keyExtractor={item => `${item.id}`}
       numColumns={2}
+      ListHeaderComponent={ListHeaderComponent}
+      ListFooterComponent={ListFooterComponent}
     />
   </View>
 )
+
+const ListHeaderComponent = () => (
+  <View style={styles.marginTop} />
+)
+
+const ListFooterComponent = () => (
+  <View style={styles.marginBottom} />
+)
+
+const styles = StyleSheet.create({
+  marginTop: {
+    marginTop: 5
+  },
+  marginBottom: {
+    marginBottom: 5
+  }
+})
 
 export default SeriesPage
