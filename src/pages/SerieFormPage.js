e381a31d368241b9aa1f8857b6c0dabd4069bc86
root <g.r.almeida@live.com>
2019-07-14 16:16:56 -0300

    Connecting SerieFormPage with Redux

--- src/pages/SerieFormPage.js
+++ src/pages/SerieFormPage.js
@@ -1,19 +1,39 @@
 import React from 'react'
 import { View } from 'react-native'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import Input from '../components/Input'
 import FormRow from '../components/FormRow'
+import { setField } from '../redux/action-creators'
 
-const SerieFormPage = (
-  <View>
-    <FormRow first>
-      <Input
-        placeholder='Title'
-        value=''
-        onChangeText={value => console.log(value)}
-      />
-    </FormRow>
-  </View>
-)
+const SerieFormPage = ({ serieForm, setField }) => {
+  const { title } = serieForm
 
-export default SerieFormPage
+  return (
+    <View>
+      <FormRow first>
+        <Input
+          placeholder='Title'
+          value={title}
+          onChangeText={value => setField('title', value)}
+        />
+      </FormRow>
+    </View>
+  )
+}
+
+SerieFormPage.propTypes = {
+  serieForm: PropTypes.object.isRequired,
+  setField: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  serieForm: state.serieForm
+})
+
+const mapDispatchToProps = {
+  setField
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SerieFormPage)
